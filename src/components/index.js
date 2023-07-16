87b5d410939e4bbd1505bf87cbaabd9052f55101
khanh nguyen <khanhjoi1@gmail.com>
2023-07-16 10:01:34 +0700

    create login componet and use auth google middleware

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as SideBar } from './SideBar';
+export { default as UserProfile } from './SideBar';
+export { default as Login } from './SideBar';
---
+++ src/container/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+
+import { SideBar, UserProfile} from '../components';
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home;
